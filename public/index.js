5cd548b17ef810990b73482e21d5f02aa5502555
Gabriek0 <gabriel.sanches170@gmail.com>
2021-10-15 22:11:08 -0300

    adicionando estilizacao e logica de verificacao de campos

--- public/index.js
+++ public/index.js
@@ -1,46 +1,82 @@
-const signIn = document.querySelector('.button-sign-in');
-const username = document.querySelector('.name');
-const email = document.querySelector('.email');
-const password = document.querySelector('.password');
-const error = document.querySelectorAll('.error');
-const successIcon = document.querySelectorAll('.success-icon');
-const failureIcon = document.querySelectorAll('.failure-icon');
+const fields = document.querySelectorAll("[required]");
 
-signIn.addEventListener('click', () => {
-    if (!username.value) {
-        error[0].innerHTML = 'Username cannot be blank';
-        username.style.border = "3px solid red";
-        failureIcon[0].style.opacity = "1"
-        successIcon[0].style.opacity = "0";
-    } else {
-        error[0].innerHTML = '';
-        username.style.border = "3px solid green";
-        successIcon[0].style.opacity = "1";
-        failureIcon[0].style.opacity = "0";
+function validateField(field) {
 
-    };
+    function verifyErrors() {
+        let foundError = false;
 
-    if (!email.value) {
-        error[1].innerHTML = 'Email cannot be blank'
-        email.style.border = "3px solid red"
-        failureIcon[1].style.opacity = "1";
-        successIcon[1].style.opacity = "0";
-    } else {
-        error[1].innerHTML = '';
-        email.style.border = "3px solid green";
-        successIcon[1].style.opacity = "1";
-        failureIcon[1].style.opacity = "0";
-    };
+        for (let error in field.validity) {
 
-    if (!password.value) {
-        error[2].innerHTML = 'Password cannot be blank'
-        password.style.border = "3px solid red";
-        failureIcon[2].style.opacity = "1";
-        successIcon[2].style.opacity = "0";
-    } else {
-        error[2].innerHTML = '';
-        password.style.border = "3px solid green";
-        successIcon[2].style.opacity = "1";
-        failureIcon[2].style.opacity = "0";
-    };
-});
+            if (field.validity[error] && !field.validity.valid) {
+                foundError = error;
+            }
+        }
+
+        return foundError;
+    }
+
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: 'Please, fill in this field.'
+            },
+            email: {
+                valueMissing: 'Email is required',
+                typeMismatch: 'Please, fill in this field.'
+            },
+            password: {
+                valueMissing: 'Password is required',
+                typeMismatch: 'Please, fill in this field.'
+
+            }
+        }
+
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector("span.error")
+
+        if (message) {
+            spanError.classList.add('active');
+            spanError.innerHTML = message;
+        } else {
+            spanError.classList.remove('active');
+            spanError.innerHTML = "";
+        }
+    }
+
+    return function () {
+
+        const error = verifyErrors();
+
+
+        if (error) {
+            const message = customMessage(error)
+
+            field.style.borderColor = ' red'
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor = 'green'
+            setCustomMessage();
+        }
+    }
+
+}
+
+function customValidation(event) {
+    const field = event.target
+
+    const validation = validateField(field);
+
+    validation();
+}
+
+for (field of fields) {
+    field.addEventListener("invalid", event => {
+
+        event.preventDefault();
+        customValidation(event);
+    })
+    field.addEventListener("blur", customValidation)
+}
